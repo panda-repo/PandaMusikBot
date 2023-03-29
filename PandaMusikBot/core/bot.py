8ade9d502082e732310ec2db926c137c89171ff2
Panda Repo <128521829+panda-repo@users.noreply.github.com>
2023-03-29 10:08:56 +0700

    Create bot.py

---
+++ PandaMusikBot/core/bot.py
@@ -0,0 +1,45 @@
+import sys
+
+from pyrogram import Client
+
+import config
+
+from ..logging import LOGGER
+
+
+
+class ShizukaBot(Client):
+    def __init__(self):
+        LOGGER(__name__).info(f"Starting Bot...")
+        super().__init__(
+            "Music",
+            api_id=config.API_ID,
+            api_hash=config.API_HASH,
+            bot_token=config.BOT_TOKEN,
+        )
+
+    async def start(self):
+        await super().start()
+        get_me = await self.get_me()
+        self.username = get_me.username
+        self.id = get_me.id
+        if get_me.last_name:
+            self.name = get_me.first_name + " " + get_me.last_name
+        else:
+            self.name = get_me.first_name
+        a = await self.get_chat_member(config.LOG_GROUP_ID, self.id)
+        if a.status != "administrator":
+            LOGGER(__name__).error(
+                "Jadikan Bot Admin di LogChat"
+            )
+            sys.exit()
+        LOGGER(__name__).info(f"MusicBot Started as {self.name}")
+        try:
+            await self.send_message(
+                config.LOG_GROUP_ID, f"**» {config.MUSIC_BOT_NAME} Bot dimulai :**\n\n🐼 ID : `{self.id}`\n🐼 ɴᴀᴍᴇ : {self.name}\n🐼 ᴜsᴇʀɴᴀᴍᴇ : @{self.username}"
+            )
+        except:
+            LOGGER(__name__).error(
+                "Bot has failed to access the log Group. Make sure that you have added your bot to your log channel and promoted as admin!"
+            )
+            sys.exit()
