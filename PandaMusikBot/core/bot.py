beb5a77050580cf64b4fd3273ba12930d29be08d
Panda Repo <128521829+panda-repo@users.noreply.github.com>
2023-03-29 12:51:50 +0700

    Update bot.py

--- PandaMusikBot/core/bot.py
+++ PandaMusikBot/core/bot.py
@@ -8,7 +8,7 @@ from ..logging import LOGGER
 
 
 
-class ShizukaBot(Client):
+class PandaBot(Client):
     def __init__(self):
         LOGGER(__name__).info(f"Starting Bot...")
         super().__init__(
