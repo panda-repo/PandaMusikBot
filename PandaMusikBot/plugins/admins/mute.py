78ecd5c70c33bfb700bff682480c0fb176e214f8
Panda Repo <128521829+panda-repo@users.noreply.github.com>
2023-03-30 16:18:16 +0700

    Update mute.py

--- PandaMusikBot/plugins/admins/mute.py
+++ PandaMusikBot/plugins/admins/mute.py
@@ -4,7 +4,7 @@ from pyrogram.types import Message
 from config import BANNED_USERS
 from strings import get_command
 from PandaMusikBot import app
-from PandaMusikBot.core.call import Shizuka
+from PandaMusikBot.core.call import Panda
 from PandaMusikBot.utils.database import is_muted, mute_on
 from PandaMusikBot.utils.decorators import AdminRightsCheck
 
@@ -22,7 +22,7 @@ async def mute_admin(cli, message: Message, _, chat_id):
     if await is_muted(chat_id):
         return await message.reply_text(_["admin_5"], disable_web_page_preview=True)
     await mute_on(chat_id)
-    await Shizuka.mute_stream(chat_id)
+    await Panda.mute_stream(chat_id)
     await message.reply_text(
         _["admin_6"].format(message.from_user.mention), disable_web_page_preview=True
     )
